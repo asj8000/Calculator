ef9268130e5f17a1bdd3199715754134153d91ce
asj8000 <asj8000@naver.com>
2021-08-31 21:42:59 +0900

    기본 레이아웃 구성 및 작동 함수 연결

--- Calculator.py
+++ Calculator.py
@@ -1,27 +1,48 @@
 import tkinter as tk
 
+def clickFunc(value):
+    if value.isdigit():
+        enterCommand(value)
+    else:
+        enterNumber(value)
+
+def enterCommand(value):
+    print(value)
+
+def enterNumber(value):
+    print(value)
+
+
+
+
+
 layout = tk.Tk()
 layout.title('계산기')
 
-entry = tk.Entry(layout, width=30)
-entry.grid(column = 0, row = 0, columnspan = 4, ipady = 20)
+entryaa = tk.Entry(layout, width=30, state="disable")
+entryaa.grid( columnspan = 4, ipady = 10, ipadx=30)
+inputEntry = tk.Entry(layout, width=30)
+inputEntry.grid( columnspan = 4, ipady = 10, ipadx=30)
 
 layoutIndex = [
-    ["1","2","3","4"],
-    ["5","6","7","8"],
-    ["9","2","3","4"]
+    ["%","CE","C","<-"],
+    ["1/x","x2","2/x","/"],
+    ["7","8","9","x"],
+    ["4","5","6","-"],
+    ["1","2","3","+"],
+    ["+/-","0",".","="]
 ]
 
 for i, layerDetail in enumerate(layoutIndex):
     for k, value in enumerate(layerDetail):
         button = tk.Button(layout, 
             text = value, 
-            width = 10, 
+            width = 10,      
             height = 3,
-            bg = 'gray',
-            fg = 'black'
+            bg = 'white',   
+            fg = 'black',
+            command = lambda 
+            val = value: clickFunc(val)
         )
-        button.grid(column = k, row = (i + 1))
-
-
+        button.grid(column = k, row = (i + 2))
 layout.mainloop()
